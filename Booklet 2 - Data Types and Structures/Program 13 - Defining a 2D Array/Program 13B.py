49f06f4a6b31a9940966d13499adc09211b1b91f
hanrahanm-ea <hanrahanm@edinburghacademy.org.uk>
2026-09-07 13:40:30 +0000

    short clear summary of changes

--- Booklet 2 - Data Types and Structures/Program 13 - Defining a 2D Array/Program 13B.py
+++ Booklet 2 - Data Types and Structures/Program 13 - Defining a 2D Array/Program 13B.py
@@ -1,16 +1,43 @@
-import numpy, pandas, random
+import numpy, pandas, random 
 rows, cols = (10,10) #define the number of rows and columns in the 2-D Array
 Game = [[" " for i in range(cols+1)] for j in range(rows+1)] #Create the empty 2-D Array using a nested loop. Note the use of +1 to account for 0th element.
 
+def move():
+    wasd = str(input("what is your move"))
 #used to set a specific element in an array
 for i in range(6): 
-    empty = False
-    Game[random.randint(1,10)][random.randint(1,10)]
-    if Game[random.randint(1,10)][random.randint(1,10)] == Game[random.randint(1,10)][random.randint(1,10)]: 
-      Game[random.randint(1,10)][random.randint(1,10)] 
-    Game[random.randint(1,10)][random.randint(1,10)] = "R"
+    empty = False 
+    randomX = random.randint(1,10)
+    randomY = random.randint(1,10)
+    while empty == False:
+        if Game[randomX][randomY] == " ":
+            empty = True 
+            Game[randomX][randomY] = "R"
+        else: 
+            empty = False
+            randomX = random.randint(1,10)
+            randomY = random.randint(1,10)
 
-Game[random.randint(1,10)][random.randint(1,10)] = "P"
+
+
+empty = False 
+randomXP = random.randint(1,10)
+randomYP = random.randint(1,10)
+while empty == False:
+        if Game[randomXP][randomYP] == " ":
+            empty = True 
+            Game[randomXP][randomYP] = "P"
+            playerX = randomXP
+            playerY = randomYP 
+        else: 
+            empty = False
+            randomX = random.randint(1,10)
+            randomY = random.randint(1,10)
+
+
+
+
+    
 
 
 
